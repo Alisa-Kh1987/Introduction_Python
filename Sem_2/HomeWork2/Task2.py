da1cf24e0570ae8b7ec5b96f7d357c28bb3c0671
Alisa <petrunina.alisa@gmail.com>
2023-08-17 02:45:16 +0300

    HW2

--- Sem_2/HomeWork2/Task2.py
+++ Sem_2/HomeWork2/Task2.py
@@ -7,4 +7,4 @@ num16 = num
 while num16 > 0:
     res_16 = str(num16 % 16) + res_16
     num16 //= 16
-print(res_16, hex(num).replace("0h", ''))
+print(res_16, hex(num).replace("0x", ''))
--- Sem_2/HomeWork2/Task6.py
+++ Sem_2/HomeWork2/Task6.py
@@ -10,4 +10,30 @@
 # операцией, даже ошибочной
 # ✔ Любое действие выводит сумму денег
 
-summ_first = 0
+# first_balance = 0
+class Bankomat():
+    def current_balance(current_balance, first_balance = 0.0):
+        current_balance = first_balance
+        
+    def add_money_to_card(current_balance):
+        # Введем счетчик количеств пополнения карты
+        count = 0        
+        add_summ = float(input("Укажите сумму пополнения карты: "))
+        if add_summ%50==0:
+            if count <=3:
+                print(f"Вы внесли сумму: {add_summ}")                
+            else:
+                add_summ = add_summ*0.97
+            count +=1
+            current_balance = add_summ + current_balance        
+            print(f"Текущий баланс карты составляет: {current_balance}")
+        else:
+            print("Необходимо внести сумму, кратную 50!")
+        
+    def output_money (current_balance):
+        output_summ = float(input("Какую сумму Вы хотите снять с карты?" ))
+        if output_summ%50==0:
+            print("Предупреждение:комиссия за снятие составляет 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.")
+
+
+    

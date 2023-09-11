033c18a76106a43ab529b76512da2595de046ddb
Alisa <petrunina.alisa@gmail.com>
2023-09-11 19:46:01 +0300

    task5_3

--- Sem_5/HomeWork5/Task5_3hw.py
+++ Sem_5/HomeWork5/Task5_3hw.py
@@ -2,11 +2,11 @@
 
 
 def fibonachi_gen(stop_generate: int):
-    fibo_list = [0, 1] # первые два числа в последовательности Фибоначчи равны, соотв., 0 и 1. Они уже вкл.чены в список
+    fibo_nums = [0, 1] # первые два числа в последовательности Фибоначчи равны, соотв., 0 и 1. Они уже вкл.чены в список
     current_num = 0
     while current_num < stop_generate:
-        yield fibo_list[-1]
-        fibo_list.append(fibo_list[-1] + fibo_list[-2])
+        yield fibo_nums[-1]
+        fibo_nums.append(fibo_nums[-1] + fibo_nums[-2])
         current_num += 1
 
 

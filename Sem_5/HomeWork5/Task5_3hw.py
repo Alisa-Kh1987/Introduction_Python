6c0defaa2266ce3c7469f2af5352f699fd0599ae
Alisa <petrunina.alisa@gmail.com>
2023-09-11 19:42:54 +0300

    task5_3hw

--- Sem_5/HomeWork5/Task5_3hw.py
+++ Sem_5/HomeWork5/Task5_3hw.py
@@ -1,12 +1,14 @@
 # Создайте функцию генератор чисел Фибоначчи
 
-def fibonachi_gen(stop_generate: int):
-    list_fibo = [0, 1]
-    current_number = 2  # первые два числа в последовательности Фибоначчи равны, соотв., 0 и 1. Они уже вкл.чены в список
-    while current_number <= stop_generate:
-        yield list_fibo[-2]
-        list_fibo.append((current_number-1)+(current_number-2))
-        current_number+=1
 
-for number in fibonachi_gen(10):
-    print(number)
+def fibonachi_gen(stop_generate: int):
+    fibo_list = [0, 1] # первые два числа в последовательности Фибоначчи равны, соотв., 0 и 1. Они уже вкл.чены в список
+    current_num = 0
+    while current_num < stop_generate:
+        yield fibo_list[-1]
+        fibo_list.append(fibo_list[-1] + fibo_list[-2])
+        current_num += 1
+
+
+for i in fibonachi_gen(10):
+    print(i)

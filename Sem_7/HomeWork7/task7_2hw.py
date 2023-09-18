aef7aa8e5d198b71909b0af70ba72581661aa78b
Alisa <petrunina.alisa@gmail.com>
2023-09-18 20:11:16 +0300

    hw7corr

--- Sem_7/HomeWork7/task7_2hw.py
+++ Sem_7/HomeWork7/task7_2hw.py
@@ -12,13 +12,13 @@
 import os
 
 
-def group_rename(new_file_name, digits, source_ext, dest_ext, range_name, path='.'):
+def group_rename(new_file_name, count_digits, source_ext, dest_ext, range_name, path='.'):
     counter = 1
     for filename in os.listdir(path):
         if filename.endswith(source_ext):
             old_name = os.path.splitext(filename)[0]
             old_name_substring = old_name[range_name[0]:range_name[1]] if range_name else ""
-            new_filename = (f'{old_name_substring}{new_file_name}{str(counter).zfill(digits)}{dest_ext}')
+            new_filename = (f'{old_name_substring}{new_file_name}{str(counter).zfill(count_digits)}{dest_ext}')
             os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
             counter += 1
 
